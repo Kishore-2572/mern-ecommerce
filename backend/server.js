d52db2a485557c8839ba0b266d987e44a995537a
Kishore-2572 <kishoren2572@gmail.com>
2022-12-02 16:05:08 +0530

    usetate to usereducer for products

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ dotenv.config()
 app.use(express.json())
 app.get('/', (req, res) =>{
     res.send("Hello")
-} )
+})
+
 app.get('/api/products', (req, res) =>{
     res.send(data.products)
-} )
+})
+
 const port=process.env.PORT
 app.listen(port, () => console.log(`Backend is running at http://localhost:${port}`))
--- frontend/src/screens/HomeScreen/homescreen.js
+++ frontend/src/screens/HomeScreen/homescreen.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import { Link } from 'react-router-dom';
 import data from '../../data.js';
 import "./homescreen.css"
+import axios from 'axios';
+import logger from 'use-reducer-logger';
 
+const reducer =(state,action) => {
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state,loading:true};
+    case 'FETCH_SUCCESS':
+      return {...state,products:action.payload,loading:false};
+    case 'FETCH_FAIL':
+      return {...state,loading:false,error:action.payload};
+    default :
+    return state;
+  }
+}
 export default function HomeScreen() {
+  const [{loading,error,products},dispatch]=useReducer(logger(reducer),{loading:true,error:'',products:[]})
+  // const [products,setProducts]=useState([]);
+  useEffect(() => {
+    const getProducts=async ()=>{
+      try{
+        dispatch({type:"FETCH_REQUEST"});
+        const result=await axios.get("/api/products");
+        // setProducts(result.data);
+        dispatch({type:"FETCH_SUCCESS",payload:result.data})
+      }catch(err){
+        dispatch({type:"FETCH_FAIL",payload:err.message})
+        console.log(err);
+      }
+    }
+    getProducts();
+  },[])
   return (
     <div>
         <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
+        {
+          loading?( <div>Loading...</div>):
+          error ? (<div>{error}</div>):
+        
+          (<div className="products">
+            {products.map((product) => (
               <div className="product" key={product.slug}>
                 <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name} />
@@ -22,7 +56,8 @@ export default function HomeScreen() {
                 </div>
               </div>
             ))}
-          </div>
+          </div>)
+          }
     </div>
   )
 }
