96f5237ce19bc492009b090504ad462119f4f4f0
Kishore-2572 <kishoren2572@gmail.com>
2022-12-02 15:22:42 +0530

    created backend folder

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const dotenv=require('dotenv')
+const data=require('./data.js')
+dotenv.config()
+
+app.use(express.json())
+app.get('/', (req, res) =>{
+    res.send("Hello")
+} )
+app.get('/api/products', (req, res) =>{
+    res.send(data.products)
+} )
+const port=process.env.PORT
+app.listen(port, () => console.log(`Backend is running at http://localhost:${port}`))
