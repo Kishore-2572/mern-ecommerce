9eed3a3941a1ef9256f6f22c8fab191e05e0e47a
Kishore-2572 <kishoren2572@gmail.com>
2022-12-02 20:27:32 +0530

    added bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,39 @@
 import './App.css';
 
-import { BrowserRouter, Route, Routes,Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen/homescreen.js';
 import ProductScreen from './screens/productscreen/productscreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App d-flex flex-column site-container">
         <header className="header">
-          <Link to="/">Amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">Amazona</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path='/' element={<HomeScreen/>} />
-            <Route path='/product/:slug' element={<ProductScreen/>} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className='text-center'>
+            All rights reserved
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
