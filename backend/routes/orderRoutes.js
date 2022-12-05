cff2e161a453c701e97e5c4434678ad3963501ca
Kishore-2572 <kishoren2572@gmail.com>
2022-12-05 12:32:39 +0530

    PayPal Implementation

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,39 +1,64 @@
 const express = require('express');
 const Order = require('../modals/orderModal');
-const bcrypt=require('bcryptjs');
-const expressAsyncHandler=require('express-async-handler');
-const {isAuth}  = require('../util');
+const bcrypt = require('bcryptjs');
+const expressAsyncHandler = require('express-async-handler');
+const { isAuth } = require('../util');
 
 const orderRouter = express.Router();
 
-
-orderRouter.post('/',isAuth,expressAsyncHandler(async (req,res) => {
-    const newOrder=new Order({
-        orderItems:req.body.orderItems.map((x) => ({...x,product:x._id})),
-        shippingAddress:req.body.shippingAddress,
-        paymentMethod:req.body.paymentMethod,
-        itemsPrice:req.body.itemsPrice,
-        shippingPrice:req.body.shippingPrice,
-        taxPrice:req.body.taxPrice,
-        totalPrice:req.body.totalPrice,
-        user:req.user._id,
-    })
-    const order=await newOrder.save();
-    res.status(201).send({message:"New order created",order});
-}))
-
+orderRouter.post(
+  '/',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const newOrder = new Order({
+      orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      user: req.user._id,
+    });
+    const order = await newOrder.save();
+    res.status(201).send({ message: 'New order created', order });
+  })
+);
 
 orderRouter.get(
-    '/:id',
-    isAuth,
-    expressAsyncHandler(async (req, res) => {
-      const order = await Order.findById(req.params.id);
-      if (order) {
-        res.send(order);
-      } else {
-        res.status(404).send({ message: 'Order Not Found' });
-      }
-    })
-  );
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
+        status: req.body.status,
+        update_time: req.body.update_time,
+        email_address: req.body.email_address,
+      };
+
+      const updatedOrder = await order.save();
+      res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
 
 module.exports = orderRouter;
