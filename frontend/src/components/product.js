8ef032ec01766eafc8141d4c6048104ac63bf978
Kishore-2572 <kishoren2572@gmail.com>
2022-12-10 16:55:26 +0530

    issued bug for componant

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -15,7 +15,7 @@ export default function Product(props) {
   const addToCartHandler = async (item) => {
     const existItem = cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${item._id}`);
+    const { data } = await axios.get(`https://long-puce-peplum.cyclic.app/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
